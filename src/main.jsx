1652a191f6df50c1d4242431a098f5c09b92333c
igunereve <igunereve@gmail.com>
2023-11-18 16:21:40 +0200

    React Card

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import Info from "./Info";
 import About from "./About";
 import Interests from "./Interests";
 import Footer from "./Footer";
+import { App } from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -12,5 +13,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <About />
     <Interests />
     <Footer />
+    <App />
   </React.StrictMode>
 );
